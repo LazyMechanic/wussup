57887a12a399897c8419574e1f459df7424a8be8
LazyMechanic <AsharnRus@gmail.com>
2021-03-10 16:20:34 +0300

    Add auth middleware on file routes

--- src/api/rest/routes/file.rs
+++ src/api/rest/routes/file.rs
@@ -9,6 +9,7 @@ fn get_files(ctx: Context) -> BoxedFilter<(impl warp::Reply,)> {
     warp::path!("v1" / "files")
         .and(warp::get())
         .and(middleware::with_context(ctx.clone()))
+        .and(middleware::with_auth(ctx))
         .and_then(handlers::file::get_files)
         .boxed()
 }
@@ -17,6 +18,7 @@ fn upload(ctx: Context) -> BoxedFilter<(impl warp::Reply,)> {
     warp::path!("v1" / "files" / "upload" / String)
         .and(warp::post())
         .and(middleware::with_context(ctx.clone()))
+        .and(middleware::with_auth(ctx))
         .and(warp::body::stream())
         .and_then(handlers::file::upload)
         .boxed()
